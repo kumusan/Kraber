596c076636ab7912c26d7e8091b01b8d5500b825
kumusan <ryogi0200@gmail.com>
2020-12-03 04:37:15 +0900

    🔱引数にシェーダーを入力

--- wasm/src/lib.rs
+++ wasm/src/lib.rs
@@ -4,6 +4,11 @@ use web_sys::{WebGlProgram, WebGlRenderingContext, WebGlShader};
 
 #[wasm_bindgen(start)]
 pub fn start() -> Result<(), JsValue> {
+    run_shader(false, "", "")
+}
+
+#[wasm_bindgen]
+pub fn run_shader(change: bool, vert_value: &str, frag_value: &str) -> Result<(), JsValue> {
     let document = web_sys::window().unwrap().document().unwrap();
     let canvas = document.get_element_by_id("canvas").unwrap();
     let canvas: web_sys::HtmlCanvasElement = canvas.dyn_into::<web_sys::HtmlCanvasElement>()?;
@@ -13,27 +18,46 @@ pub fn start() -> Result<(), JsValue> {
         .unwrap()
         .dyn_into::<WebGlRenderingContext>()?;
 
-    let vert_shader = compile_shader(
-        &context,
-        WebGlRenderingContext::VERTEX_SHADER,
-        r#"
-        attribute vec4 position;
-        void main() {
-            gl_Position = position;
-        }
-    "#,
-    )?;
-    let frag_shader = compile_shader(
-        &context,
-        WebGlRenderingContext::FRAGMENT_SHADER,
-        r#"
-        void main() {
-            gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);
-        }
-    "#,
-    )?;
-    let program = link_program(&context, &vert_shader, &frag_shader)?;
-    context.use_program(Some(&program));
+    //////////////////////////////////////////////////////////////////////
+    // 今後最適化
+    if !change {
+        let vert_shader = compile_shader(
+            &context,
+            WebGlRenderingContext::VERTEX_SHADER,
+            r#"
+            attribute vec4 position;
+            void main() {
+                gl_Position = position;
+            }
+        "#,
+        )?;
+        let frag_shader = compile_shader(
+            &context,
+            WebGlRenderingContext::FRAGMENT_SHADER,
+            r#"
+            void main() {
+                gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);
+            }
+        "#,
+        )?;
+        let program = link_program(&context, &vert_shader, &frag_shader)?;
+        context.use_program(Some(&program));
+    } else {
+        let vert_shader = compile_shader(
+            &context,
+            WebGlRenderingContext::VERTEX_SHADER,
+            vert_value,
+        )?;
+        let frag_shader = compile_shader(
+            &context,
+            WebGlRenderingContext::FRAGMENT_SHADER,
+            frag_value,
+        )?;
+        let program = link_program(&context, &vert_shader, &frag_shader)?;
+        context.use_program(Some(&program));
+    }
+
+    ///////////////////////////////////////////////////////////////////////////////
 
     let vertices: [f32; 9] = [-0.7, -0.7, 0.0, 0.7, -0.7, 0.0, 0.0, 0.7, 0.0];
 
@@ -119,7 +143,12 @@ pub fn add_test(a: u32, b: u32) -> u32 {
     a + b
 }
 
-// #[wasm_bindgen]
-// pub fn rerendering() -> Result<(), JsValue> {
-   
-// }
+#[wasm_bindgen]
+pub fn rerendering(size: &str) -> Result<(), JsValue> {
+    
+    let document = web_sys::window().unwrap().document().unwrap();
+    let h = document.get_element_by_id("wasmcall").unwrap();
+    h.set_inner_html(size);
+
+    Ok(())
+}
